b13a601cf6b545c8c4db6fd4a6181d3fee6ce679
Luiz Bissoni <labissoni@gmail.com>
2020-05-01 15:38:05 -0300

    init

---
+++ OpenEmulatorAndroid.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+
+cd "$($env:LOCALAPPDATA)\Android\Sdk\emulator" 
+
+$lista = @(./emulator.exe -list-avds)
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($lista)
+
+$title = 'Executar emulador'
+$message = if($lista.Count -gt 1) {'Emuladores disponíveis'} else {'Emulador disponível'}
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+
+./emulator.exe -avd $lista[$result]
